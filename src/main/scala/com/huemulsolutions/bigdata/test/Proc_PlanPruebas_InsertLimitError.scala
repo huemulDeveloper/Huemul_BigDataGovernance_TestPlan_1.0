3305ffbbece9984326c2cb8af5f1d0c2b3f34d7c
Sebastián Rodríguez Robotham <sebas_rod@hotmail.com>
2018-08-26 02:50:07 -0300

    cambia clase

--- src/main/scala/com/huemulsolutions/bigdata/test/Proc_PlanPruebas_InsertLimitError.scala
+++ src/main/scala/com/huemulsolutions/bigdata/test/Proc_PlanPruebas_InsertLimitError.scala
@@ -12,7 +12,7 @@ object Proc_PlanPruebas_InsertLimitError {
   def main(args: Array[String]): Unit = {
     val huemulLib = new huemul_Library("01 - Plan pruebas error en insert por límite de filas",args,globalSettings.Global)
     val Control = new huemul_Control(huemulLib,null)
-    
+    //huemulLib.spark
     val Ano = huemulLib.arguments.GetValue("ano", null,"Debe especificar ano de proceso: ejemplo: ano=2017")
     val Mes = huemulLib.arguments.GetValue("mes", null,"Debe especificar mes de proceso: ejemplo: mes=12")
     
@@ -30,10 +30,23 @@ object Proc_PlanPruebas_InsertLimitError {
       }
       
       Control.NewStep("Mapeo de Campos")
-      val TablaMaster = new tbl_DatosBasicosLimitRow(huemulLib, Control)      
+      val TablaMaster = new tbl_DatosBasicosNuevos(huemulLib, Control)      
       TablaMaster.DataFramehuemul.setDataFrame(DF_RAW.DataFramehuemul.DataFrame, "DF_Original")
       
+      //BORRA HDFS ANTIGUO PARA EFECTOS DEL PLAN DE PRUEBAS
+      val a = huemulLib.spark.catalog.listTables(TablaMaster.GetCurrentDataBase()).collect()
+      if (a.filter { x => x.name.toUpperCase() == TablaMaster.TableName.toUpperCase()  }.length > 0) {
+        huemulLib.spark.sql(s"drop table if exists ${TablaMaster.GetTable()} ")
+      } 
       
+      val FullPath = new org.apache.hadoop.fs.Path(s"${TablaMaster.GetFullNameWithPath()}")
+      val fs = FileSystem.get(huemulLib.spark.sparkContext.hadoopConfiguration) 
+      if (fs.exists(FullPath))
+        fs.delete(FullPath, true)
+        
+   //BORRA HDFS ANTIGUO PARA EFECTOS DEL PLAN DE PRUEBAS
+      
+    
       TablaMaster.TipoValor.SetMapping("TipoValor",true,"coalesce(new.TipoValor,'nulo')","coalesce(new.TipoValor,'nulo')")
       TablaMaster.IntValue.SetMapping("IntValue")
       TablaMaster.BigIntValue.SetMapping("BigIntValue")
@@ -48,17 +61,42 @@ object Proc_PlanPruebas_InsertLimitError {
       
       Control.NewStep("PASO 1: INSERTA NORMAL")
       if (!TablaMaster.executeFull("DF_Final_Todo")) {
-        IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "No hay error en masterización", "No hay error en masterización", s"Si hay error en masterización", true)
+        IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "No hay error en masterización", "No hay error en masterización", s"Si hay error en masterización", false)
         Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)     
         Control.RaiseError(s"Error al masterizar (${TablaMaster.Error_Code}): ${TablaMaster.Error_Text}")
+      } else {
+        IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "No hay error en masterización", "No hay error en masterización", s"No hay error en masterización", true)
+        Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)
+      }
+      TablaMaster.DataFramehuemul.DataFrame.show()
+       Control.NewStep("Define DataFrame Original")
+      
+      if (!DF_RAW.open("DF_RAW", null, Ano.toInt, Mes.toInt, 1, 0, 0, 0,"Nuevos")) {
+        Control.RaiseError(s"Error al intentar abrir archivo de datos: ${DF_RAW.Error.ControlError_Message}")
+      }
+      TablaMaster.DataFramehuemul.setDataFrame(DF_RAW.DataFramehuemul.DataFrame, "DF_Nuevos")
+      
+      val DFValidaCantIni = huemulLib.DF_ExecuteQuery("validaCantidad", s"select cast(count(1) as Long) as Cantidad from ${TablaMaster.GetTable()}")
+      val NumReg = DFValidaCantIni.first().getAs[Long]("Cantidad")
+      IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "NumRegInicial", "N° Registros iniciales de tablas", "N° Reg Inicial = 6", s"N° Reg Inicial = ${NumReg}", NumReg == 6)
+      Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)
+        
+      Control.NewStep("PASO 1: INSERTA NUEVOS")
+      if (!TablaMaster.executeOnlyInsert("DF_Final_Todo")) {
+        IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "Si hay error en masterización", "Si hay error en masterización", s"Si hay error en masterización", true)
+        Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)     
       } else {
         IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "No hay error en masterización", "No hay error en masterización", s"No hay error en masterización", false)
         Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)
       }
-      
-      IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "No hay error en masterización", "Si hay error en masterización 1005", s"Si hay error en masterización (${TablaMaster.Error_Code})", TablaMaster.Error_Code == 1005)
+      TablaMaster.DataFramehuemul.DataFrame.show()
+      IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "Masterización", "Si hay error en masterización", "Si hay error en masterización 1005", s"Si hay error en masterización (${TablaMaster.Error_Code})", TablaMaster.Error_Code == 1005)
       Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)
       
+      val DFValidaCantFin = huemulLib.DF_ExecuteQuery("validaCantidad", s"select cast(count(1) as Long) as Cantidad from ${TablaMaster.GetTable()}")
+      val NumRegFin = DFValidaCantFin.first().getAs[Long]("Cantidad")
+      IdTestPlan = Control.RegisterTestPlan(TestPlanGroup, "NumRegFinal", "N° Registros Finales de tablas", "N° Reg Finales = 6", s"N° Reg Finales = ${NumRegFin}", NumRegFin == 6)
+      Control.RegisterTestPlanFeature("DQ_MaxNewRecords_Num", IdTestPlan)
       
           Control.FinishProcessOK
     } catch {
